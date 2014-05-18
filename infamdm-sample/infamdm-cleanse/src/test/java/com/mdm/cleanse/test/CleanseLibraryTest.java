ca0b0679af498ab51d125f2318a1d73d64b166b1
P25 <P25@GIGABYTE>
2014-05-19 08:22:59 +0900

    test source modified

--- infamdm-sample/infamdm-cleanse/src/test/java/com/mdm/cleanse/test/CleanseLibraryTest.java
+++ infamdm-sample/infamdm-cleanse/src/test/java/com/mdm/cleanse/test/CleanseLibraryTest.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import com.mdm.cleanse.base.FunctionUnit;
+import com.mdm.cleanse.base.LoggerHelper;
 import com.mdm.cleanse.base.SMCCleanseLibrary;
 import com.mdm.cleanse.base.UnifiedCleanseFunction;
 import com.siperian.mrm.cleanse.api.CleanseException;
@@ -17,30 +18,46 @@ public class CleanseLibraryTest {
 	
 	private final Logger logger = Logger.getLogger(getClass());
 	
-	public static void main(String[] args) {
-		try {
-			new CleanseLibraryTest().test();
-		} catch (CleanseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	
+	@Test
+	public void testRRN() throws CleanseException{
+		
+		String funcName = "QSA_CN_SSNCHK";
+		Map<String,Object> output = execTest(funcName, "750231", "2178423");
+		LoggerHelper.debugLog(logger, "=== Result of {} ===", funcName);
+		for(String key : output.keySet()){
+			LoggerHelper.debugLog(logger, "{} : [{}]", key, output.get(key));
 		}
+		LoggerHelper.debugLog(logger, "=================");
 	}
 	
 	@Test
-	public void test() throws CleanseException{
-		
+	public void testURL() throws CleanseException {
+
+		String funcName = "QSA_CN_URLCHK";
+		Map<String,Object> output = execTest(funcName, "http://www.yahoo.com");
+		LoggerHelper.debugLog(logger, "=== Result of {} ===", funcName);
+		for(String key : output.keySet()){
+			LoggerHelper.debugLog(logger, "{} : [{}]", key, output.get(key));
+		}
+		LoggerHelper.debugLog(logger, "=================");
+	}
+	
+	private Map<String,Object> execTest(String funcName,Object... values) throws CleanseException{
 		SMCCleanseLibrary library = new SMCCleanseLibrary();
 		library.initialize(new Properties());
 		Map<String,Object> context = new HashMap<String,Object>();
 		Map<String,Object> input = new HashMap<String,Object>();
 		Map<String,Object> output = new HashMap<String,Object>();
 		
-		UnifiedCleanseFunction func = (UnifiedCleanseFunction)library.getCleanseFunction("QSA_CN_SSNCHK");
+		UnifiedCleanseFunction func = (UnifiedCleanseFunction)library.getCleanseFunction(funcName);
 		FunctionUnit unit = func.getFunctionUnit();
-		input.put(unit.getInputNames()[0], "751232");
-		input.put(unit.getInputNames()[1], "1120612");
+		int idx = 0;
+		for(Object val : values){
+			input.put(unit.getInputNames()[idx++], val);
+		}
 		
 		func.cleanse(context, input, output);
-		logger.debug("result : " + output.get(unit.getOutputNames()[3]));
+		return output;
 	}
 }
